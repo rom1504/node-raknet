0669a519f993b17dbec6f829c4372647012027e5
Keegan <mhsjlw@aol.com>
2016-02-17 18:10:35 -0500

    finally add DataPackets

--- src/datatypes.js
+++ src/datatypes.js
@@ -76,10 +76,135 @@ function sizeOfRestBuffer(value) {
   return value.length;
 }
 
+function readEncapsulatedPacket(buffer, offset, typeArgs) {
+  var packet = {};
+  var size;
+  var flags = buffer[offset];
+
+  packet.reliability = ((flags & 0b11100000) >> 5);
+  packet.hasSplit = (flags & 0b00010000) > 0;
+  offset+=1;
+  size+=1;
+
+  var length, offset;
+
+  if (typeArgs.internal) {
+    length = length = this.read(buffer, offset, "int").value;
+    offset+=4;
+    size+=4;
+    packet.identifierACK = this.read(buffer, offset, "int").value;
+    offset+=4;
+    size+=4;
+  } else {
+    length = (int) Math.ceil(((double) this.read(buffer, offset, "short").value) / 8));
+    offset+=3;
+    size+=3;
+    packet.identifierACK = null;
+  }
+
+  if (packet.reliability > 0) {
+    if (packet.reliability >= 2 && packet.reliability != 5) {
+        packet.messageIndex = this.read(buffer, offset, "ltriad").value;
+        offset+=3;
+        size+=3;
+      }
+
+      if (packet.reliability <= 4 && packet.reliability != 2) {
+        packet.orderIndex = this.read(buffer, offset, "ltriad").value;
+        offset+=3;
+        size+=3;
+        packet.orderChannel = buffer[offset];
+      }
+  }
+
+  if (packet.hasSplit) {
+    packet.splitCount = this.read(buffer, offset, "int").value;
+    offset+=4;
+    size+=4
+    packet.splitID = this.read(buffer, offset, "short").value;
+    offset+=2;
+    size+=2;
+    packet.splitIndex = this.read(buffer, offset, "int").value;
+    offset+=4;
+    size+=4;
+  }
+
+  packet.buffer = buffer;
+  offset += length;
+  packet.offset = offset;
+
+  return {
+    value: packet,
+    size: size
+  }
+}
+
+function writeEncapsulatedPacket(value, buffer, offset, typeArgs) {
+  offset=this.write((value.reliability << 5) | (value.hasSplit ? 0b00010000 : 0),buffer, offset, "byte");
+  
+  if (typeArgs.internal) {
+    offset=this.write(buffer.length, buffer, offset, "int");
+    offset=this.write((value.identifierACK == null ? 0 : value.identifierACK), buffer, offset, "int");
+  } else {
+    offset=this.write(buffer.length << 3, buffer, offset, "short");
+  }
+
+  if (value.reliability > 0) {
+    if (value.reliability >= 2 && value.reliability != 5) {
+      offset=this.write((value.messageIndex == null ? 0 : value.messageIndex), buffer, offset, "ltriad");
+    }
+    if (value.reliability <= 4 && value.reliability != 2) {
+      offset=this.write(value.orderIndex, buffer, offset, "ltriad");
+      offset=this.write(value.orderChannel & 0xff, buffer, offset, "byte");
+    }
+  }
+
+  if (value.hasSplit) {
+    offset=this.write(value.splitCount, buffer, offset, "int");
+    offset=this.write(value.splitID, buffer, offset, "short");
+    offset=this.write(value.splitIndex, buffer, offset, "int");
+  }
+
+  offset=this.write(value.buffer, buffer, offset, "buffer");
+
+  return offset;
+}
+
+function sizeOfEncapsulatedPacket(value, typeArgs) {
+  var size=0;
+  size+=this.sizeOf((value.reliability << 5) | (value.hasSplit ? 0b00010000 : 0),buffer, "byte");
+  
+  if (typeArgs.internal) {
+    size+=this.sizeOf(buffer.length, buffer, "int");
+    size+=this.sizeOf((value.identifierACK == null ? 0 : value.identifierACK), buffer, "int");
+  } else {
+    size+=this.sizeOf(buffer.length << 3, buffer, "short");
+  }
+
+  if (value.reliability > 0) {
+    if (value.reliability >= 2 && value.reliability != 5) {
+      size+=this.sizeOf((value.messageIndex == null ? 0 : value.messageIndex), buffer, "ltriad");
+    }
+    if (value.reliability <= 4 && value.reliability != 2) {
+      size+=this.sizeOf(value.orderIndex, buffer, "ltriad");
+      size+=this.sizeOf(value.orderChannel & 0xff, buffer, "byte");
+    }
+  }
+
+  if (value.hasSplit) {
+    size+=this.sizeOf(value.splitCount, buffer, "int");
+    size+=this.sizeOf(value.splitID, buffer, "short");
+    size+=this.sizeOf(value.splitIndex, buffer, "int");
+  }
+
+  return size;
+}
+
 module.exports = {
   'magic': [readMagic, writeMagic, 16],
   'ipAddress': [readIpAddress, writeIpAddress, 4],
   'triad': [readTriad, writeTriad, 3],
   'ltriad': [readLTriad, writeLTriad, 3],
   'restBuffer': [readRestBuffer, writeRestBuffer, sizeOfRestBuffer],
+  'EncapsulatedPacket': [readEncapsulatedPacket, writeEncapsulatedPacket, sizeOfEncapsulatedPacket]
 };
